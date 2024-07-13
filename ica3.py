12587eb2bfca9c301bc95e0814deea586c55ed78
Dave Dowling <dave@dowlingdata.com>
2024-07-13 12:02:48 -0400

    rename assignment, add /data, /out

--- ica3.py
+++ ica3.py
@@ -1,19 +1,15 @@
-filename = './lego_setsHB.csv'
+filename = './data/lego_setsHB.csv'
 counter=0
 price = {}
 difficulty = {}
 with open(filename, 'r') as fl:
     next(fl)
     for line in fl:
-        #print(line)
         tokens = line.split(',')
-        #for el in tokens:
-        #    print(el, end='--')
-        #print()
         price[tokens[4]] = tokens[0]
         difficulty[tokens[4]] = tokens[5]
         counter+=1
-        if counter==10:
+        if counter==1000:
           break
 for key, value in price.items():
     print(key, value, difficulty[key])
