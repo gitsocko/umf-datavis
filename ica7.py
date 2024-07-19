649f738788aba2032c1d5a164148f475e53b334c
gitstocko <dave_d1968@yahoo.com>
2024-07-19 17:04:03 -0400

    ica7

---
+++ ica7.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+boxoffice = pd.read_csv('./data/boxoffice.csv')
+print(boxoffice.head())
+swAmt = boxoffice.loc[boxoffice['title'] == 'Star Wars: The Last Jedi', 'amount'].iloc[0]
+print(swAmt)
+
+plt.figure(figsize=(15, 3))
+plt.bar(boxoffice['title'],boxoffice['amount'])
+plt.show() # ./out/ica7a.png
+
+boRkDsc=boxoffice.sort_values(by='rank', ascending=False)
+print(boRkDsc.head())
+
+plt.figure(figsize=(15, 3))
+plt.bar(boRkDsc['title'],boRkDsc['amount_text']+"M")
+plt.title('Ticket Sales (in millions)')
+plt.show() # ./out/ica7b.png
+
+plt.figure(figsize=(10, 3))
+plt.bar(boRkDsc['title_short'],boRkDsc['amount_text']+"M")
+plt.title('Ticket Sales (in millions), Short Titles')
+plt.show() # ./out/ica7c.png
